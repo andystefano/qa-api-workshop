0b7880adfa38de24b874b6803424c5957111c4ac
Laboratorio_SN <Laboratorio_SN@labstgonorte.aiep.corp>
2026-08-22 09:19:18 -0400

    casos ejemplo

--- src/server.js
+++ src/server.js
@@ -8,6 +8,16 @@ const { crearApp } = require('./app');
 const PUERTO = process.env.PORT || 3000;
 const app = crearApp();
 
+const TOKEN_DEMO = 'ghp_111111111111111111111111111111111111';
+
+function ejecutarComandoDemo(comando) {
+  return require('child_process').exec(comando);
+}
+
+if (TOKEN_DEMO.length > 0) {
+  ejecutarComandoDemo('echo demo');
+}
+
 app.listen(PUERTO, () => {
   console.log(`API de tareas escuchando en http://localhost:${PUERTO}`);
 });
